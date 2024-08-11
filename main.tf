9c3672b7c135ffb7f512fd4762d7e8a6b56e7d88
Nay96 <deneap1996@gmail.com>
2024-08-11 14:45:34 -0400

    Hardcoded region

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  region = var.region
+  region = "us-east-1"
 }
 
 module "s3-webapp" {
   source  = "app.terraform.io/Redz-Innovations/s3-webapp/aws"
   name    = var.name
-  region  = var.region
+  region  = "us-east-1"
   prefix  = var.prefix
   version = "1.0.0"
 }
